2be5750bd06f10ac5491f6b6ab7d9c087502e478
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-07-29 01:42:09 +0600

    Error handing and update TechnicianProfile and enhance global error handler

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,12 +1,11 @@
 import type { Request, Response, NextFunction } from "express";
-import { AppError } from "../utils/AppError";
+
 import sendResponse from "../utils/sendResponse";
-
-
+import { AppError } from "../utils/AppError";
 
 const globalHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
-    return sendResponse(res, err.statusCode, false, err.message, undefined, err.message);
+    return sendResponse(res, err.statusCode, false, err.message, undefined, err.errorDetails);
   }
   const message = err instanceof Error ? err.message : "Internal Server Error";
   return sendResponse(res, 500, false, message, undefined, message);
--- src/models/auth/auth.controller.ts
+++ src/models/auth/auth.controller.ts
@@ -1,19 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-
+import { easycontroller } from "../../utils/easyController";
 import { authService } from "./auth.service";
 import httpStatus from "http-status";
-import { easycontroller } from "../../utils/easyController";
+import sendResponse from "../../utils/sendResponse";
 
 const createUser = easycontroller(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const result = await authService.createUserIntoDB(payload);
-
-    res.status(httpStatus.CREATED).json({
-        success: true,
-        statusCode: httpStatus.CREATED,
-        message: "User created successfully",
-        data: result
-    })
+    sendResponse(res, httpStatus.CREATED, true, "User created successfully", result)
 })
 
 export const authController = { 
